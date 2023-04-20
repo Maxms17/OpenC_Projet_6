91cccab92378661b972b8aad5d2edf68825fe7e3
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-04-20 16:10:46 +0200

    Modification script

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,9 +1,9 @@
 
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('../Backend/projet.json');
-const projet = await reponse.json();
+const reponse = await fetch("http://localhost:5678/api/works");
+const works = await reponse.json();
 
-const mesprojet = projet[0];
+const mesprojet = works[0];
 
 const image = document.createElement("img");
 image.src = mesprojet.image;
@@ -14,10 +14,3 @@ text.innerText = mesprojet.text;
 const sectionPortfolio = document.querySelector(".portfolio");
 sectionPortfolio.appendChild(image);
 sectionPortfolio.appendChild(text);
-
-/*
-for(i=0;i<11;i++){
-    img_projet.src = `./assets/images/${projet[i].image}`
-	text_projet.innerHTML = projet[i].text
-}
-*/
