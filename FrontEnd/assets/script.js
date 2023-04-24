47aa205dc3e6992e2bd55a0e072e12ae5818d014
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-04-24 15:44:58 +0200

    Ajout css bouton et JS

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -26,3 +26,45 @@ async function getData() {
 
 getData()
 
+const boutonTrierTous = document.querySelector(".button_trier_Tous");
+
+boutonTrierTous.addEventListener("click", function () {
+    console.log("Tous");
+});
+
+const boutonTrierObj = document.querySelector(".button_trier_Obj");
+
+boutonTrierObj.addEventListener("click", function () {
+    console.log("Objet");
+});
+
+const boutonTrierApp = document.querySelector(".button_trier_App");
+
+boutonTrierApp.addEventListener("click", function () {
+    console.log("Appartement");
+});
+
+const boutonTrierHetR = document.querySelector(".button_trier_HetR");
+
+boutonTrierHetR.addEventListener("click", function () {
+    console.log("Hotel et restaurant");
+});
+
+//const boutonTrierTous = document.querySelector(".button_trier_Tous");
+
+//boutonTrierTous.addEventListener("click", function () {
+    //pieces.sort(function (a, b) {
+        //return a.prix - b.prix;
+    //});
+    //console.log(pieces);
+//});
+
+//const boutonTrierObj = document.querySelector(".button_trier_Obj");
+
+//boutonTrierObj.addEventListener("click", function () {
+    //const objet = Array.from(works);
+    //objet.sort(function (a, b) {
+        //return objet;
+    //});
+    //console.log(objet);
+//});
