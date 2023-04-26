411f3c9570f1725fb837543d07ef449937e604cc
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-04-26 11:53:06 +0200

    Modification script.js

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,29 +1,68 @@
-
 const boutonTrierTous = document.querySelector(".button_trier_Tous");
 const boutonTrierObj = document.querySelector(".button_trier_Obj");
 const boutonTrierApp = document.querySelector(".button_trier_App");
 const boutonTrierHetR = document.querySelector(".button_trier_HetR");
 
+
+async function getTous() {
+    const reponse = await fetch("http://localhost:5678/api/works");
+    const works = await reponse.json();
+
+    works.map((item) => {
+        console.log(item)
+        const sectionPortfolio = document.querySelector(".gallery");
+
+        const carte = document.createElement("article");
+
+        const image = document.createElement("img");
+        image.src = item.imageUrl;
+
+        const text = document.createElement("p");
+        text.innerText = item.title;
+
+        carte.classList.add("article")
+        carte.dataset.cat = item.category.name
+        sectionPortfolio.appendChild(carte);
+
+        carte.appendChild(image);
+        carte.appendChild(text);
+    })
+
+}
+
+getTous();
+
+
+function handlefilter(cat) {
+    const articles = document.querySelectorAll('.article');
+    
+    articles.forEach(item => {
+        if (item.dataset.cat !== cat) {
+            item.style.display = "none"
+        } else {
+            item.style.display = "block"
+        }
+    })
+    if (cat === "Tous") {
+        articles.forEach(item => {
+            item.style.display = "block"
+        })
+    }
+}
+
+
 boutonTrierTous.addEventListener("click", function () {
-    getNul() 
-    getTous()
-    console.log("Tous");
+    handlefilter("Tous")
 });
 
 boutonTrierObj.addEventListener("click", function () {
-    getNul() 
-    getObj()
-    console.log("Objet");
+    handlefilter("Objets")
 });
 
 boutonTrierApp.addEventListener("click", function () {
-    getNul() 
-    getApp()
-    console.log("Appartement");
+    handlefilter("Appartements")
 });
 
 boutonTrierHetR.addEventListener("click", function () {
-    getNul() 
-    getHetR()
-    console.log("Hotel et restaurant");
+    handlefilter("Hotels & restaurants")
 });
--- FrontEnd/assets/script_nul.js
+++
@@ -1,5 +0,0 @@
-
-function getNul() {
-    // Efface le contenu de la balise body et donc l’écran
-    document.querySelector(".gallery").innerHTML = '';
-}
