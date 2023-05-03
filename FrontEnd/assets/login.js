e89dbcb32c1aa9df688451627b687180611b5af8
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-05-03 18:15:13 +0200

    Modification ajout banniere edition

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,50 +2,52 @@ function ajoutListenerConnection() {
     const login = document.querySelector(".login");
     login.addEventListener("submit", function (event) {
         event.preventDefault(); // éviter que la page ne se recharge
-  
+
         const connection = {
             email: event.target.querySelector("[name=email]").value,
             password: event.target.querySelector("[name=mdp]").value,
         };
-  
+
         const donneConnection = JSON.stringify(connection);
-  
+
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { "Content-Type": "application/json", "Origin": "http://localhost:5500/" },
-         body: donneConnection,
-         })
-        /*
-        .then((res) => res.json())
-        .then((res) => {
-            if (res.status === 200) {
-                window.location.href = "index.html";
-                return res.json(); // Si le statut est 200, parsez le JSON et continuez
-            } else {
-                throw new Error(`erreur: ${res.status}`); // Sinon, lancez une erreur avec le code de statut
-            }
+            body: donneConnection,
         })
-        */
         .then((res) => {
-            if (!res.ok) {
-                throw new Error(`Erreur HTTP ${res.status}`);
+            if(res.status === 200){
+                document.getElementById("error-message").innerHTML = "Vous êtes autorisé à vous connecter.";
+                return res.json();
             }
-            return res.json();
+            else if(res.status === 401){
+                document.getElementById("error-message").innerHTML = "Vous n'êtes pas autorisé à vous connecter.";
+            }
+            else if(res.status === 404){
+                document.getElementById("error-message").innerHTML = "Une erreur s'est produite lors de la connexion. Vérifier votre email et votre mot de passe.";
+            }
+            else{
+                document.getElementById("error-message").innerHTML = "Une erreur s'est produite lors de la connexion. Veuillez réessayer plus tard.";
+            }
         })
         .then((data) => {
-            const token = data.token; // Extrait le token du JSON
-            sessionStorage.setItem("token", token);
-            window.location.href = "index.html";
-            // Stockez le token et effectuez d'autres actions ici
+            if (data) { 
+                const token = data.token; // Extrait le token du JSON
+                sessionStorage.setItem("token", token);
+      
+                const element = document.getElementById("edition");
+                element.style.display = "flex";
+      
+                window.location.href = "index.html";
+            }
         })
         .catch((error) => {
             console.error(error);
             document.getElementById("error-message").innerHTML = "Une erreur s'est produite lors de la connexion. Veuillez réessayer plus tard.";
-            //throw error; // Propager l'erreur pour afficher un message d'erreur dans la console du navigateur
         });
     });
 }
-  
+
 ajoutListenerConnection();
 
 /*
