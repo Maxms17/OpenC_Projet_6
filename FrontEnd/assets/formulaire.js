96575e9dc34f23443b38ee89688b21871ac4732f
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-05-04 15:23:34 +0200

    Ajout fichier formulaire

---
+++ FrontEnd/assets/formulaire.js
@@ -0,0 +1,34 @@
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector("#openModal");
+const close = document.querySelector(".close");
+
+openModal.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+close.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+/*
+function modale(){
+
+}
+
+modale();
+function supprimer_modale(){
+
+}
+
+supprimer_modale();
+function ajouter_modale(){
+
+}
+
+ajouter_modale();
+*/
