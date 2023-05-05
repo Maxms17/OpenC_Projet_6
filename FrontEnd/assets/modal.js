58eeecc57ecd64717c8f1edb50b9ab763d3380ce
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-05-05 15:05:27 +0200

    Ajout galerie dans le modal

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -2,8 +2,35 @@ const modal = document.querySelector(".modal");
 const openModal = document.querySelector("#openModal");
 const close = document.querySelector(".close");
 
+async function getEdition() {
+  const reponse = await fetch("http://localhost:5678/api/works");
+  const works = await reponse.json();
+
+  works.map((item) => {
+      console.log(item)
+      const sectionPortfolio = document.querySelector(".galleryModal");
+
+      const carte = document.createElement("article");
+
+      const image = document.createElement("img");
+      image.src = item.imageUrl;
+
+      const text = document.createElement("p");
+      text.innerText = "éditer" ;
+
+      carte.classList.add("article")
+      carte.dataset.cat = item.category.name
+      sectionPortfolio.appendChild(carte);
+
+      carte.appendChild(image);
+      carte.appendChild(text);
+  })
+
+}
+
 openModal.addEventListener("click", function() {
   modal.style.display = "block";
+  getEdition();
 });
 
 close.addEventListener("click", function() {
