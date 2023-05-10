df9a39d7192edf981daa893f87c0d384c2198f70
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-05-10 13:35:30 +0200

    ajout retour

--- FrontEnd/assets/ajouter.js
+++ FrontEnd/assets/ajouter.js
@@ -2,5 +2,36 @@
 const ajoutModal = document.querySelector("#ajoutModal");
 
 ajoutModal.addEventListener("click", function () {
-  console.log("ajouter modal");
+
+  const modal_deux = document.querySelector(".modal_deux");
+  modal_deux.style.display = "block";
+  //visibilityEdition("Visible");
+  modal.style.display = "none";
+  //visibilityEdition("nonVisible")
+
+  const close_deux = document.querySelector(".close_deux");
+
+  close_deux.addEventListener("click", function() {
+    modal_deux.style.display = "none";
+    //visibilityEdition("nonVisible");
+    modal.style.display = "block";
+    //visibilityEdition("Visible")
+  });
+  
+  const retour = document.querySelector(".retour");
+
+  retour.addEventListener("click", function() {
+    modal_deux.style.display = "none";
+    //visibilityEdition("nonVisible");
+    modal.style.display = "block";
+    //visibilityEdition("Visible")
+  });
+
+  window.addEventListener("click", function(event) {
+    if (event.target == modal_deux) {
+      modal_deux.style.display = "none";
+    }
+  });
+
 });
+
